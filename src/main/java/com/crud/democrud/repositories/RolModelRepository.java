66e3694466888be7b5db385137dd8dc6ea406e50
Adryan <adryanynfante@gmail.com>
2022-04-27 11:25:49 -0500

    creacion de repositorio modelo Rol
    creacion de repositorio  modelo rol

---
+++ src/main/java/com/crud/democrud/repositories/RolModelRepository.java
@@ -0,0 +1,7 @@
+package com.crud.democrud.repositories;
+
+import com.crud.democrud.models.RolModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolModelRepository extends JpaRepository<RolModel, Long> {
+}
